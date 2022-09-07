fe5f934fad865e8f32dd410d3fa11b92e1f50747
lukeh990 <lukeharding990@gmail.com>
2022-09-07 19:47:52 -0400

    Reimplement all features

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/sites", express.static(path.join(__dirname, 'sites')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
